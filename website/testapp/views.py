772127c45967d1b543a176b2dd0b3c18308dcf21
Alexandr4 <4upkob@gmail.com>
2016-04-14 23:52:18 +0300

    Update views.py

--- website/testapp/views.py
+++ website/testapp/views.py
@@ -3,4 +3,4 @@ from datetime import datetime
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home2.html', {'right_now':datetime.utcnow()})
+    return render(request, 'home.html', {'right_now':datetime.utcnow()})
